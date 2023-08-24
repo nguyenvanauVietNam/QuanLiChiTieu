c3d629aa1ace981f0907d90404fd6fd081701b12
nguyenvanauVietNam <honguyentenau@gmail.com>
2023-08-24 21:29:26 +0700

    Update GUI connnect Database

--- ConnectionDataBase/MainWindow.xaml.cs
+++ ConnectionDataBase/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,12 @@ namespace ConnectionDataBase
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
-            InitializeComponent();
+
         }
+
     }
 }
